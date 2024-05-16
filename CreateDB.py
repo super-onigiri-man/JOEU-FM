3c16ef208f181a4cc2d86feedff5de763165da4f
Shunsuke <syunsuke030828@gmail.com>
2024-05-16 14:33:05 +0900

    DBに独自IDを設定

--- CreateDB.py
+++ CreateDB.py
@@ -20,17 +20,18 @@ try:
         Last_Rank INT,
         Last_Number INT,
         On_Chart INT,
-        PRIMARY KEY (Title, Artist)
-    );"""
+        Unique_id TEXT,
+        PRIMARY KEY (Unique_id)
+        );"""
     #↑DBのフォーマット設定（別のところに書いておきます）
     cursor.execute(sql)#executeコマンドでSQL文を実行
     conn.commit()#データベースにコミット(Excelでいう上書き保存。自動コミット設定なので不要だが一応・・)
-    with open('楽曲データ.csv',encoding='utf-8') as f:
+    with open('result.csv',encoding='utf-8') as f:
         # CSVリーダーオブジェクトを作成
         csv_reader = csv.reader(f)
         for row in csv_reader:
             # テーブルにデータを挿入
-            cursor.execute("INSERT INTO music_master VALUES (?,?,?,?,?,?)", (row[0],row[1],row[2],row[3],row[4],row[5]))
+            cursor.execute("INSERT INTO music_master VALUES (?,?,?,?,?,?,?)", (row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
 
 
 except Exception as e:
