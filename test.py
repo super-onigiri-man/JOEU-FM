ae7e2188fc303ff28192eb9780b7345b33589a4e
super-onigiri-man <syunsuke030828@gmail.com>
2024-05-21 01:21:15 +0900

    独自データをCSVに追加するためのコード

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-try:
-    #コードをここに書くと発生する例外がファイルに出力される。
-    print(あああ) # あああ　は文字列変数を指定していなければ"(ダブルクォート）で囲む必要がある。
-except Exception as e:
-    import traceback
-    with open('error.log', 'a') as f:
-        traceback.print_exc( file=f)
+import psutil
+for proc in psutil.process_iter():
+    print("----------------------")
+    print("プロセスID:" + str(proc.pid))
+    try:
+        print("実行モジュール：" + proc.exe())
+        print("コマンドライン:" + str(proc.cmdline()))
+        print("カレントディレクトリ:" + proc.cwd())
+    except psutil.AccessDenied:
+        print("このプロセスへのアクセス権がありません。")
